04fa2d916e65eb9127b22183f55680ddf944b416
Srijan619 <chapssrijan@gmail.com>
2020-03-15 17:21:37 +0200

    Updates

---
+++ precache-manifest.0b661e6c890dbd5b48902f3bd97a755e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12d7cac07065391e3872864d1e6febdf",
+    "url": "/FPL-API/index.html"
+  },
+  {
+    "revision": "8e43874a3633223f118b",
+    "url": "/FPL-API/static/css/main.4befff33.chunk.css"
+  },
+  {
+    "revision": "37ccff606b77fd2ebb75",
+    "url": "/FPL-API/static/js/2.9d16e4a8.chunk.js"
+  },
+  {
+    "revision": "e7df5ec6c9986765193824c7f118c284",
+    "url": "/FPL-API/static/js/2.9d16e4a8.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8e43874a3633223f118b",
+    "url": "/FPL-API/static/js/main.cf889ff2.chunk.js"
+  },
+  {
+    "revision": "75b4a3f9f486bf4444e8",
+    "url": "/FPL-API/static/js/runtime-main.8b53ff86.js"
+  }
+]);
